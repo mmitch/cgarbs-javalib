0670e24fae2d082de4f6bd9a55c09d94bd982500
Christian Garbs <mitch@cgarbs.de>
2015-11-29 22:39:39 +0100

    add path split methods to FileUtils
    these are reverse operations of those methods already present

--- src/main/java/de/cgarbs/lib/FileUtils.java
+++ src/main/java/de/cgarbs/lib/FileUtils.java
@@ -5,6 +5,7 @@
 package de.cgarbs.lib;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 /**
  * File-related utility functions.
@@ -21,23 +22,60 @@ public abstract class FileUtils
 	 *
 	 * @param elements The path elements to concatenate.  Null values are skipped.
 	 * @return the concatenated path
+	 * @see #splitPath(String) splitPath() for the reverse operation
 	 */
-	public static String createPathFrom(final String... elements )
+	public static String createPathFrom(final String... elements)
 	{
 		return StringUtils.join(File.separator, elements);
 	}
 
 	/**
 	 * Creates a new {@link File} from the concatination
-	 * Cof several path elements and an optional
+	 * of several path elements and an optional
 	 * final filename.
 	 *
 	 * @param elements The path elements to concatenate.  Null values are skipped.
 	 * @return a new {@link File} pointing to the concatenated path
+	 * @see #splitFile(File) splitFile()for the reverse operation
 	 */
-	public static File createFileFrom(final String... elements )
+	public static File createFileFrom(final String... elements)
 	{
 		return new File(createPathFrom(elements));
 	}
 
+	/**
+	 * Splits a String containing a path and an optional final filename
+	 * separated by {@link File#separatorChar} into individual path elements.
+	 *
+	 *
+	 * @param path The path to split. A null results in an empty array.
+	 * @return an array consisting of the individual elements of the path
+	 * @see #createPathFrom(String...) createPathFrom() for the reverse operation
+	 */
+	public static String[] splitPath(final String path)
+	{
+		if (path == null)
+		{
+			return null;
+		}
+		return path.split(Pattern.quote(File.separator), -1);
+	}
+
+	/**
+	 * Splits the a path and an optional final filename of a File
+	 * into individual path elements.
+	 *
+	 *
+	 * @param file The File whose path should be split. A null results in an empty array.
+	 * @return an array consisting of the individual elements of the File's path
+	 * @see #createFileFrom(String...) createFileFrom() for the reverse operation
+	 */
+	public static String[] splitFile(final File file)
+	{
+		if (file == null)
+		{
+			return null;
+		}
+		return splitPath(file.getPath());
+	}
 }
--- src/test/java/de/cgarbs/lib/FileUtilsTest.java
+++ src/test/java/de/cgarbs/lib/FileUtilsTest.java
@@ -6,6 +6,7 @@ package de.cgarbs.lib;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 import java.io.File;
@@ -18,6 +19,11 @@ public class FileUtilsTest
 	public void checkCreatePathFrom()
 	{
 		final char sep = File.separatorChar;
+
+		assertThat(
+				FileUtils.createPathFrom(null),
+				is(equalTo(""))
+				);
 
 		assertThat(
 				FileUtils.createPathFrom(),
@@ -53,6 +59,53 @@ public class FileUtilsTest
 		assertThat(
 				FileUtils.createPathFrom("","foo", ""),
 				is(equalTo(sep+"foo"+sep))
+				);
+	}
+
+	@Test
+	public void checkSplitPath()
+	{
+		final char sep = File.separatorChar;
+
+		assertThat(
+				FileUtils.splitPath(null),
+				is(nullValue())
+				);
+
+		assertThat(
+				FileUtils.splitPath(""),
+				is(equalTo(new String[]{""}))
+				);
+
+		assertThat(
+				FileUtils.splitPath("foo"),
+				is(equalTo(new String[]{"foo"}))
+				);
+
+		assertThat(
+				FileUtils.splitPath("foo"+sep+"bar"+sep+"baz"),
+				is(equalTo(new String[]{"foo", "bar", "baz"}))
+				);
+
+		// check empty Path components
+		assertThat(
+				FileUtils.splitPath(sep+"foo"),
+				is(equalTo(new String[]{"", "foo"}))
+				);
+
+		assertThat(
+				FileUtils.splitPath("foo"+sep+sep+sep+sep+"bar"),
+				is(equalTo(new String[]{"foo", "", "", "", "bar"}))
+				);
+
+		assertThat(
+				FileUtils.splitPath("foo"+sep),
+				is(equalTo(new String[]{"foo", ""}))
+				);
+
+		assertThat(
+				FileUtils.splitPath(sep+"foo"+sep),
+				is(equalTo(new String[]{"","foo", ""}))
 				);
 	}
 
@@ -95,6 +148,56 @@ public class FileUtilsTest
 		assertThat(
 				FileUtils.createFileFrom("","foo", ""),
 				is(equalTo(new File(sep+"foo"+sep)))
+				);
+	}
+
+	@Test
+	public void checkSplitFile()
+	{
+		final char sep = File.separatorChar;
+
+		assertThat(
+				FileUtils.splitFile(null),
+				is(nullValue())
+				);
+
+		assertThat(
+				FileUtils.splitFile(new File("")),
+				is(equalTo(new String[]{""}))
+				);
+
+		assertThat(
+				FileUtils.splitFile(new File("foo")),
+				is(equalTo(new String[]{"foo"}))
+				);
+
+		assertThat(
+				FileUtils.splitFile(new File("foo"+sep+"bar"+sep+"baz")),
+				is(equalTo(new String[]{"foo", "bar", "baz"}))
+				);
+
+		// check empty Path components
+		assertThat(
+				FileUtils.splitFile(new File(sep+"foo")),
+				is(equalTo(new String[]{"", "foo"}))
+				);
+
+		assertThat(
+				FileUtils.splitFile(new File("foo"+sep+sep+sep+sep+"bar")),
+//				is(equalTo(new String[]{"foo", "", "", "", "bar"}))
+				is(equalTo(new String[]{"foo", "bar"}))  // note: File shortens away multiple separators
+				);
+
+		assertThat(
+				FileUtils.splitFile(new File("foo"+sep)),
+//				is(equalTo(new String[]{"foo", ""}))
+				is(equalTo(new String[]{"foo"})) // WTF: no "trailing slash" possible with File?
+				);
+
+		assertThat(
+				FileUtils.splitFile(new File(sep+"foo"+sep)),
+//				is(equalTo(new String[]{"","foo", ""}))
+				is(equalTo(new String[]{"","foo"})) // WTF: no "trailing slash" possible with File?
 				);
 	}
 }
