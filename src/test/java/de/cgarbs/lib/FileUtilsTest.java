68f21b63f5e494e0eb950459d9af39adabfc578d
Christian Garbs <mitch@cgarbs.de>
2015-11-29 21:45:22 +0100

    additional FileUtils tests for empty path components

--- src/test/java/de/cgarbs/lib/FileUtilsTest.java
+++ src/test/java/de/cgarbs/lib/FileUtilsTest.java
@@ -33,6 +33,27 @@ public class FileUtilsTest
 				FileUtils.createPathFrom("foo", "bar", "baz"),
 				is(equalTo("foo"+sep+"bar"+sep+"baz"))
 				);
+
+		// check empty Path components
+		assertThat(
+				FileUtils.createPathFrom("", "foo"),
+				is(equalTo(sep+"foo"))
+				);
+
+		assertThat(
+				FileUtils.createPathFrom("foo", "", "", "", "bar"),
+				is(equalTo("foo"+sep+sep+sep+sep+"bar"))
+				);
+
+		assertThat(
+				FileUtils.createPathFrom("foo", ""),
+				is(equalTo("foo"+sep))
+				);
+
+		assertThat(
+				FileUtils.createPathFrom("","foo", ""),
+				is(equalTo(sep+"foo"+sep))
+				);
 	}
 
 	@Test
@@ -53,6 +74,27 @@ public class FileUtilsTest
 		assertThat(
 				FileUtils.createFileFrom("foo", "bar", "baz"),
 				is(equalTo(new File("foo"+sep+"bar"+sep+"baz")))
+				);
+
+		// check empty Path components
+		assertThat(
+				FileUtils.createFileFrom("", "foo"),
+				is(equalTo(new File(sep+"foo")))
+				);
+
+		assertThat(
+				FileUtils.createFileFrom("foo", "", "", "", "bar"),
+				is(equalTo(new File("foo"+sep+sep+sep+sep+"bar")))
+				);
+
+		assertThat(
+				FileUtils.createFileFrom("foo", ""),
+				is(equalTo(new File("foo"+sep)))
+				);
+
+		assertThat(
+				FileUtils.createFileFrom("","foo", ""),
+				is(equalTo(new File(sep+"foo"+sep)))
 				);
 	}
 }
